d8f3783cc8599a7172c81b9cc75843a0cf1b4135
sashatsyhanov <alexandertsyhanov@gmail.com>
2026-03-28 16:57:00 +0300

    а

--- src/app/app/rentals/new/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import PropertyForm from "@/components/units/PropertyForm";
-
-export default function NewRentalPage() {
-  return <PropertyForm category="rent" />;
-}
--- src/app/app/sale/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function LegacySalePage() {
-  redirect("/app/sales/secondary");
-}
-
